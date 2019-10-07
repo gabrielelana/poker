0add02caef702b3d1445b4b45f52e04749cdd596
gabriele.lana <gabriele.lana@gmail.com>
2019-10-07 18:06:00 +0200

    Deck implementation

--- lib/deck.ex
+++ lib/deck.ex
@@ -1,2 +1,31 @@
 defmodule Poker.Deck do
+  alias Poker.Card
+
+  @type t :: [Card.t()]
+
+  @cards_in_deck 5
+  @cards for rank <- Card.ranks(), suit <- Card.suits(), do: {rank, suit}
+
+  @spec new() :: t
+  def new do
+    @cards
+  end
+
+  @spec shuffle(t) :: t
+  def shuffle(deck, seed \\ :os.timestamp()) do
+    :random.seed(seed)
+    Enum.shuffle(deck)
+  end
+
+  @spec deal_to(t, number) :: [Hand.t()]
+  def deal_to(deck, number_of_players) when number_of_players in 2..6 do
+    shuffle(deck)
+    |> Enum.chunk_every(number_of_players)
+    |> Enum.take(@cards_in_deck)
+    |> transpose
+  end
+
+  defp transpose([]), do: []
+  defp transpose([[] | _]), do: []
+  defp transpose(l), do: [Enum.map(l, &hd/1) | transpose(Enum.map(l, &tl/1))]
 end
--- test/deck_test.exs
+++ test/deck_test.exs
@@ -3,15 +3,15 @@ defmodule Poker.DeckTest do
 
   alias Poker.Deck
 
-  # test "shuffle/1 a deck of cards" do
-  #   deck = Deck.new()
-  #   assert Deck.shuffle(deck, 1) != Deck.shuffle(deck, 2)
-  # end
+  test "shuffle/1 a deck of cards" do
+    deck = Deck.new()
+    assert Deck.shuffle(deck, 1) != Deck.shuffle(deck, 2)
+  end
 
-  # test "deal hands of cards to a number of players" do
-  #   deck = Deck.new()
-  #   [h1, h2] = Deck.deal_to(deck, 2)
-  #   assert length(h1) == 5
-  #   assert length(h2) == 5
-  # end
+  test "deal hands of cards to a number of players" do
+    deck = Deck.new()
+    [h1, h2] = Deck.deal_to(deck, 2)
+    assert length(h1) == 5
+    assert length(h2) == 5
+  end
 end
