9f222ad69a4fcae5dc3872fe72be4f1211a1bbb1
gabriele.lana <gabriele.lana@gmail.com>
2019-10-07 18:07:16 +0200

    Hand implementation

--- lib/hand.ex
+++ lib/hand.ex
@@ -33,6 +33,45 @@ defmodule Poker.Hand do
   end
 
   # @spec do_identify([{number, Card.rank()}]) :: {kind_of_hand, tuple}
+  defp do_identify([{r1, s}, {r2, s}, {r3, s}, {r4, s}, {r5, s}])
+       when [r2 - r1, r3 - r2, r4 - r3, r5 - r4] == [1, 1, 1, 1],
+       do: {:straight_flush, {9, r5}}
+
+  defp do_identify([{r1, _}, {r1, _}, {r1, _}, {r1, _}, {r2, _}]),
+    do: {:four_of_a_kind, {8, r1, r2}}
+
+  defp do_identify([{r1, _}, {r2, _}, {r2, _}, {r2, _}, {r2, _}]),
+    do: {:four_of_a_kind, {8, r2, r1}}
+
+  defp do_identify([{r1, _}, {r1, _}, {r1, _}, {r2, _}, {r2, _}]), do: {:full_house, {7, r1, r2}}
+  defp do_identify([{r2, _}, {r2, _}, {r1, _}, {r1, _}, {r1, _}]), do: {:full_house, {7, r1, r2}}
+
+  defp do_identify([{_, s}, {_, s}, {_, s}, {_, s}, {r5, s}]), do: {:flush, {6, r5}}
+
+  defp do_identify([{r1, _}, {r2, _}, {r3, _}, {r4, _}, {r5, _}])
+       when [r2 - r1, r3 - r2, r4 - r3, r5 - r4] == [1, 1, 1, 1],
+       do: {:straight, {5, r5}}
+
+  defp do_identify([{r1, _}, {r1, _}, {r1, _}, {_, _}, {_, _}]), do: {:three_of_a_kind, {4, r1}}
+  defp do_identify([{_, _}, {r2, _}, {r2, _}, {r2, _}, {_, _}]), do: {:three_of_a_kind, {4, r2}}
+  defp do_identify([{_, _}, {_, _}, {r3, _}, {r3, _}, {r3, _}]), do: {:three_of_a_kind, {4, r3}}
+
+  defp do_identify([{r1, _}, {r1, _}, {r2, _}, {r2, _}, {r3, _}]),
+    do: {:two_pair, {3, r2, r1, r3}}
+
+  defp do_identify([{r1, _}, {r2, _}, {r2, _}, {r3, _}, {r3, _}]),
+    do: {:two_pair, {3, r3, r2, r1}}
+
+  defp do_identify([{r1, _}, {r1, _}, {r2, _}, {r3, _}, {r3, _}]),
+    do: {:two_pair, {3, r3, r1, r2}}
+
+  defp do_identify([{r1, _}, {r1, _}, {_, _}, {_, _}, {r4, _}]), do: {:one_pair, {2, r1, r4}}
+  defp do_identify([{_, _}, {r2, _}, {r2, _}, {_, _}, {r4, _}]), do: {:one_pair, {2, r2, r4}}
+  defp do_identify([{_, _}, {_, _}, {r3, _}, {r3, _}, {r4, _}]), do: {:one_pair, {2, r3, r4}}
+  defp do_identify([{_, _}, {_, _}, {r3, _}, {r4, _}, {r4, _}]), do: {:one_pair, {2, r4, r3}}
+
+  defp do_identify([{r1, _}, {r2, _}, {r3, _}, {r4, _}, {r5, _}]),
+    do: {:high_card, {1, r5, r4, r3, r2, r1}}
 
   defp aces_as_ones(hand) do
     hand
